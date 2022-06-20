8c3d798fb9070b57a54ca9ff4016168056b2e418
ShayG0 <86621168+ShayG0@users.noreply.github.com>
2022-06-20 15:55:28 +0300

    home page layout

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div
+      className="text-center d-flex flex-column"
+      style={{ display: "inline-block" }}
+    >
+      <div style={{ display: "inline-block" }}>
+        <h1 className="cover-heading">Welcome To The Zoo</h1>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/8/8a/Blank_Park_Zoo_Entrance.jpg"
+          alt="hadera picturer"
+          height="auto"
+          width="500"
+        />
+      </div>
+    </div>
+  );
 }

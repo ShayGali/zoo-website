1086d7d8900e761cee0a0f870bbcfca40c14a327
ShayGali <shayali100@gmail.com>
2022-06-27 11:58:23 +0300

    make the image on the home responsive

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,13 +6,17 @@ export default function HomePage() {
       className="text-center d-flex flex-column"
       style={{ display: "inline-block" }}
     >
-      <div style={{ display: "inline-block" }}>
-        <h1 className="cover-heading">Welcome To The Zoo</h1>
+      <h1 className="cover-heading">Welcome To The Zoo</h1>
+      <div>
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/8/8a/Blank_Park_Zoo_Entrance.jpg"
           alt="hadera picturer"
-          height="auto"
-          width="500"
+          style={{
+            width: "550px",
+            height: "auto",
+            "max-width": "100%",
+            margin: "25px",
+          }}
         />
       </div>
     </div>

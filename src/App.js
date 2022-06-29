b390ef3d2e1ae262ca99e9b7818bff1e64cb2eb6
ShayGali <shayali100@gmail.com>
2022-06-29 15:44:31 +0300

    update

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import AnimalDetails from "./components/AnimalDetails";
 import AddAnimal from "./pages/AddAnimalPage";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes basename="Zoo-Website">
+    <BrowserRouter basename="/Zoo-Website">
+      <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
 
